2b4cd022c351c1e1d374ae78d6d90ba46a2b658a
Shaktiraj Das <shaktiraj.das@quocent.com>
2022-10-11 12:25:41 +0530

    ios updated code

---
+++ Airapay/AirapayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirapayApp.swift
+//  Airapay
+//
+//  Created by Quocent on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AirapayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
